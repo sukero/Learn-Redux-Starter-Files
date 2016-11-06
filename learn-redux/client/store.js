3db38b645dd16b0e1e4c175c481092fdf61ea63f
helloberries <helloberriesqz@gmail.com>
2016-11-06 23:20:21 +0800

    #6

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -9,3 +9,13 @@ import comments from './data/comments';
 import posts from './data/posts';
 
 // create an object for the default data
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
