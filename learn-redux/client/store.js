495a227f2e5d7d27fa466ac66edfe7577aee850f
helloberries <helloberriesqz@gmail.com>
2016-11-06 21:46:53 +0800

    #5

---
+++ learn-redux/client/store.js
@@ -0,0 +1,11 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
